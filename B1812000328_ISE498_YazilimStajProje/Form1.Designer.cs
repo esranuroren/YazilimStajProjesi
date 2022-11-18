804559fbabc247b180fa7d753151433874fcca10
esranuroren <118452294+esranuroren@users.noreply.github.com>
2022-11-18 12:21:41 +0300

    Add files via upload

---
+++ B1812000328_ISE498_YazilimStajProje/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace B1812000328_ISE498_YazilimStajProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDersID = new System.Windows.Forms.TextBox();
+            this.txtDersKodu = new System.Windows.Forms.TextBox();
+            this.txtDersAdi = new System.Windows.Forms.TextBox();
+            this.txtDersYariyili = new System.Windows.Forms.TextBox();
+            this.txtDersiVerenler = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DersID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ders Kodu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ders Adı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ders Yarıyılı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dersi Verenler";
+            // 
+            // txtDersID
+            // 
+            this.txtDersID.Enabled = false;
+            this.txtDersID.Location = new System.Drawing.Point(154, 57);
+            this.txtDersID.Name = "txtDersID";
+            this.txtDersID.Size = new System.Drawing.Size(186, 20);
+            this.txtDersID.TabIndex = 5;
+            // 
+            // txtDersKodu
+            // 
+            this.txtDersKodu.Location = new System.Drawing.Point(154, 94);
+            this.txtDersKodu.Name = "txtDersKodu";
+            this.txtDersKodu.Size = new System.Drawing.Size(186, 20);
+            this.txtDersKodu.TabIndex = 6;
+            // 
+            // txtDersAdi
+            // 
+            this.txtDersAdi.Location = new System.Drawing.Point(154, 126);
+            this.txtDersAdi.Name = "txtDersAdi";
+            this.txtDersAdi.Size = new System.Drawing.Size(186, 20);
+            this.txtDersAdi.TabIndex = 7;
+            // 
+            // txtDersYariyili
+            // 
+            this.txtDersYariyili.Location = new System.Drawing.Point(154, 159);
+            this.txtDersYariyili.Name = "txtDersYariyili";
+            this.txtDersYariyili.Size = new System.Drawing.Size(186, 20);
+            this.txtDersYariyili.TabIndex = 8;
+            // 
+            // txtDersiVerenler
+            // 
+            this.txtDersiVerenler.Location = new System.Drawing.Point(154, 188);
+            this.txtDersiVerenler.Name = "txtDersiVerenler";
+            this.txtDersiVerenler.Size = new System.Drawing.Size(186, 20);
+            this.txtDersiVerenler.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 252);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(644, 186);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(489, 60);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(122, 23);
+            this.btnEkle.TabIndex = 11;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(489, 105);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(122, 23);
+            this.btnSil.TabIndex = 12;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(489, 148);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(122, 23);
+            this.btnGuncelle.TabIndex = 13;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtDersiVerenler);
+            this.Controls.Add(this.txtDersYariyili);
+            this.Controls.Add(this.txtDersAdi);
+            this.Controls.Add(this.txtDersKodu);
+            this.Controls.Add(this.txtDersID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDersID;
+        private System.Windows.Forms.TextBox txtDersKodu;
+        private System.Windows.Forms.TextBox txtDersAdi;
+        private System.Windows.Forms.TextBox txtDersYariyili;
+        private System.Windows.Forms.TextBox txtDersiVerenler;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
+
---
+++ B1812000328_ISE498_YazilimStajProje/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+
+//Sql Server bağlantısı yapabilmek için gerekli kütüphaneyi ekliyorum
+using System.Data.SqlClient;
+
+
+
+
+
+namespace B1812000328_ISE498_YazilimStajProje
+{
+
+    //B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+    public partial class Form1 : Form
+    {
+        //Bağlantı için kullanacağım nesnelerimi oluşturuyorum
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter DA;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+
+        //Verilerde yaptığım ekleme - silme - güncelleme işlemlerini görüntülemek ve tekrarlı komut yazmamak için 
+        //metodu formload dışına yazıyorum
+
+
+        void DersGetir()
+        {
+
+            baglanti = new SqlConnection("server=.; Initial Catalog= B181200028_Ise498_YazilimStajProje; Integrated Security=SSPI");
+
+            //bağlantıyı oluşturduktan sonra açıyorum
+            baglanti.Open();
+
+            //data adaptörünü ayarlıyorum
+            DA = new SqlDataAdapter("SELECT *FROM Dersler", baglanti);
+
+            //Datatable kullanarak veriyi dataGridView'a çekiyorum
+            DataTable tablo = new DataTable();
+            DA.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+
+            //bağlantıyı kapatıyorum
+            baglanti.Close();
+        }
+
+
+
+        //B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DersGetir();
+        }
+         
+        //seçtiğim satırların textboxlara gelmesini sağlıyorum
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtDersID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtDersKodu.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtDersAdi.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtDersYariyili.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtDersiVerenler.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        //B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+
+        //Veri ekleme butonumun kodlarını ve sql server sorgusunu yazıyorum
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            //tabloya veri eklerken DersID alanı otomatik artan olduğu için ona müdahale etmiyo ve insert sorgumun 
+            //içine yazmıyorum
+
+            string sorgu = "INSERT INTO Dersler(DersKodu, DersAdı, DersYarıyılı, DersiVerenler)" +
+                " VALUES (@DersKodu, @DersAdı, @DersYarıyılı, @DersiVerenler)";
+            komut = new SqlCommand(sorgu, baglanti);
+
+            //parametrelerimi ayarlıyorum
+            komut.Parameters.AddWithValue("@DersKodu", txtDersKodu.Text);
+            komut.Parameters.AddWithValue("@DersAdı", txtDersAdi.Text);
+            komut.Parameters.AddWithValue("@DersYarıyılı", txtDersYariyili.Text);
+            komut.Parameters.AddWithValue("@DersiVerenler", txtDersiVerenler.Text);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            //sql serverda tablomda yapılan değişiklikleri görüntülemek için dersgetir metodumu çağırıyorum yeniden
+            DersGetir();
+
+        }
+
+
+        //B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+
+        //veri silme butonumun kodlarını ve sql server sorgusunu yazıyorum
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            //veri silme işleminde sql server sorgusunun yanlış yazılması durumunda tüm verinin silinmesi münkün olduğu
+            //için tablodan sadece seçtiğim satırı silebilmek için sorguya where komutunu ekliyorum
+            string sorgu = "DELETE FROM Dersler WHERE DersID=@DersID";
+            komut = new SqlCommand(sorgu, baglanti);
+
+            //parametremi ayarlıyorum
+            komut.Parameters.AddWithValue("@DersID", Convert.ToInt32(txtDersID.Text));
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            DersGetir();
+        }
+
+        //B181200028 Esranur ÖREN ISE498 Yazılım Stajı Projesi
+
+        //veri güncelleme butonumun kodlarını ve sql server sorgusunu yazıyorum
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            //veri güncelleme işlemini DersID'ye göre uygulamak için sql server sorgusunda 
+            //yeniden where komutunu kullanıyorum
+            string sorgu = "UPDATE Dersler SET DersKodu=@DersKodu, DersAdı=@DersAdı, DersYarıyılı=@DersYarıyılı," +
+                " DersiVerenler=@DersiVerenler WHERE DersID=@DersID";
+
+            komut = new SqlCommand(sorgu, baglanti);
+
+            //parametrelerimi ayarlıyorum
+            komut.Parameters.AddWithValue("@DersID", Convert.ToInt32(txtDersID.Text));
+            komut.Parameters.AddWithValue("@DersKodu", txtDersKodu.Text);
+            komut.Parameters.AddWithValue("@DersAdı", txtDersAdi.Text);
+            komut.Parameters.AddWithValue("@DersYarıyılı", txtDersYariyili.Text);
+            komut.Parameters.AddWithValue("@DersiVerenler", txtDersiVerenler.Text);
+
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            DersGetir();
+        }
+
+        
+    }
+}
